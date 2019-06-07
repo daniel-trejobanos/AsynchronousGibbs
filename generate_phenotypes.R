a8d96793edde4430ee24ef03fca114a2e52685c8
Daniel Trejo banos <dtrejoba@dbc-serv04.vital-it.ch>
2019-06-07 15:07:38 +0200

    working on coefficient definitions

--- generate_phenotypes.R
+++ generate_phenotypes.R
@@ -1,10 +1,10 @@
 N <- 10000 #number of individuals
- 
+M <- c(600,300,100 )
 data.path <-  '/scratch/temporary/dtrejoba/AsynchronousGibbs'
 #we generate the covariate matrix fo r the first RAR matrix
 require(mvnfast)
 
-corrMatrices <- list.files(path=data.path,full.names=T,pattern="dataR.*.RData")
+corrMatrices <- list.files(path=data.path,full.names=T,pattern="*10k50k*")
 print("reading list of correlation matrices")
 print(corrMatrices)
 
@@ -13,13 +13,22 @@ create.x.matrix <- function( corr.matrix ){
 }
 
 create.b.matrix <- function( x.matrix, name=NULL){
-       if(name!=NULL){
+       if( length(grep( "RARb" ,name) ) ==0 ){
+            B <- rep(0,ncol(x.matrix ))
+            coefficients <- sample(1:ncol(x.matrix),size = sum(M) )
+            comps <- list()
+            for(i in 1: length(M)){
+                tmp <- sample(coefficients,size = M[i])
+                comps <- append(comps,list(tmp))
+                coefficients <-  setdiff(coefficients,comps[[i]])
+            }
             
+                
        }#special case for the block matrix
        else{
-           
+           load()
        }
-
+       list(coeff = , comp = )
 }
 
 gen_lm <- function(matrixName){
@@ -32,7 +41,7 @@ gen_lm <- function(matrixName){
 	beta <- create.b.matrix(X,corr.matrix)
         b <- beta$coeff
         print("generating i.i.d noise")
-        e <- rnorm(N,sqrt())
+        e <- rnorm(N,sqrt(0.5))
         print("building linear model")
         g <- X %*% b$coeff
         y <- g + e
